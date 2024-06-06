56733395ee987111b95e44784d9290ae00115969
Pablo Montemor <pablo.montemor@usp.br>
2024-06-06 17:11:14 -0300

    initial: arquivos iniciais do projeto

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F7C57D',
     alignItems: 'center',
     justifyContent: 'center',
   },
