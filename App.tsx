3d045c76cac6b1d8cbff21ca09e19a507d196ba5
Pablo Montemor <pablo.montemor@usp.br>
2024-06-06 17:40:37 -0300

    feat: adiciona componentes na tela de login

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+const { height: screenHeight, width: screenWidth } = Dimensions.get('window');
+
+function RetanguloCinza({ children }) {
+  const rectangleHeight = screenHeight * 0.8; 
+  const rectangleWidth = screenWidth * 0.8;
+  return (
+    <View style={[styles.retanguloCinza, { height: rectangleHeight, width: rectangleWidth }]}>
+      {children}
+    </View>
+  );
+}
+
+
+function InputUsuario({}) {
+  const rectangleHeight = screenHeight * 0.1; 
+  const rectangleWidth = screenWidth * 0.7;
+  return (
+    <View style={[styles.inputUsuario, { height: rectangleHeight, width: rectangleWidth }]} />
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <RetanguloCinza>
+        <InputUsuario />
+        <InputUsuario />
+      </RetanguloCinza>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +42,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#F7C57D',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  retanguloCinza: {
+    backgroundColor: '#d3d1d2',
+    borderRadius: 20,
+    margin: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputUsuario: {
+    backgroundColor: '#ffffff',
+    borderRadius: 20,
+    margin: 20,
   },
 });
