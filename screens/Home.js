0849a87501e958c32d7ed9b609b8c512c8c7e3f7
Pablo Montemor <pablo.montemor@usp.br>
2024-06-08 13:18:26 -0300

    feat: adiciona componentes de header na Home

--- screens/Home.js
+++ screens/Home.js
@@ -1,29 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, ImageBackground, Link, Pressable } from 'react-native';
 
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 import ProductCarousel from '../components/ProductCarousel';
 
 
-
 export default function Home() {
   return (
-
     <View style={styles.container}>
-        <Header />
-          <Text>Ofertas</Text>
-          <Text>Ver mais</Text>
-          <ProductCarousel />
-        <FooterMenu/>
+      <Header />
+      <View style={styles.headerRow}>
+        <Text style={styles.headerTitle}>Ofertas</Text>
+        <Pressable>
+          <Text style={styles.headerLink}>Ver mais</Text>
+        </Pressable>
+      </View>
+      <ProductCarousel />
+
+      <View style={styles.headerRow}>
+        <Text style={styles.headerTitle}>Cookies</Text>
+        <Pressable>
+          <Text style={styles.headerLink}>Ver mais</Text>
+        </Pressable>
+      </View>
+      <FooterMenu />
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#EDEDED',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#EDEDED',
+  },
+  headerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  headerLink: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'orange',
+  },
 });
