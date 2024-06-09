452fe34e91c2db95981b4335373d7afc0214ae6a
thelua <theluamaia@gmail.com>
2024-06-09 19:51:28 -0300

    att da qntd de pedido

--- components/CookieCart.js
+++ components/CookieCart.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 //fonte poppins
 
@@ -10,14 +10,24 @@ const CookieCard = () => {
     document.head.appendChild(link);
   }, []);
 
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => setQuantity(quantity + 1);
+  const decreaseQuantity = () => setQuantity(quantity > 1 ? quantity - 1 : 1);
+
   return (
     <div style={styles.container}>
       <div style={styles.card}>
-      <img src="../assets/cookie-icon.png" alt="Cookie Icon" style={styles.cookieIcon} />
+        <img src="../assets/cookie-icon.png" alt="Cookie Icon" style={styles.cookieIcon} />
         <div style={styles.content}>
           <h2 style={styles.title}>Tradicional Gotas de Chocolate</h2>
           <p style={styles.description}>Massa tradicional com deliciosas gotas de chocolate ao leite</p>
           <p style={styles.price}>R$ 10,00</p>
+        </div>
+        <div style={styles.quantityContainer}>
+          <button style={styles.quantityButton} onClick={decreaseQuantity}>-</button>
+          <span style={styles.quantity}>{quantity}</span>
+          <button style={styles.quantityButton} onClick={increaseQuantity}>+</button>
         </div>
       </div>
     </div>
@@ -43,6 +53,7 @@ const styles = {
     width: '90%',
     maxWidth: '400px',
     boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
+    position: 'relative', 
   },
   cookieIcon: {
     width: '66px', 
@@ -79,8 +90,37 @@ const styles = {
     fontFamily: 'Poppins, sans-serif',
     textAlign: 'center',
   },
+  quantityContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    right: '-15px', 
+    bottom: '-15px',
+    backgroundColor: '#FCB040',
+    borderRadius: '100%',
+    padding: '5px 10px',
+    boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
+    width: '44px', 
+    height: '54px',
+  },
+  quantityButton: {
+    backgroundColor: 'transparent',
+    border: 'none',
+    borderRadius: '50%',
+    width: '30px',
+    height: '30px',
+    fontSize: '18px',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    margin: '0 5px',
+  },
+  quantity: {
+    fontSize: '18px',
+    fontWeight: 'bold',
+    color: 'black',
+    fontFamily: 'arial',
+  },
 };
 
 export default CookieCard;
-
-
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import Header from '../components/Header';
+import CookieCard from '../components/CookieCart';
 
 
 export default function Profile() {
@@ -9,6 +10,7 @@ export default function Profile() {
   return (
     <View>
       <Header />
+      <CookieCard />
 
     </View>
   );
